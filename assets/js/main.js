6cd3507a23d2b816c2c03eced5d39d0f27f86c41
Cléber Severo <clebersevero1998@gmail.com>
2022-11-09 00:20:44 -0300

    improving js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
 const selecionar = document.querySelector(".formulario__seleciona")
 const btnAdicionar = document.getElementById('formulario__adicionar')
+const entradaDescricao = document.getElementById('descricao')
+const entradaValor = document.getElementById('valor')
+const lista = document.getElementById('lista')
 
+var despesas = []
 
 
 
@@ -8,13 +12,44 @@ btnAdicionar.addEventListener('click', (e) => {
 
     e.preventDefault()
 
-    
-    if(selecionar.value == 1) {
-        console.log("é positivo")
-    } else {
-        console.log("é negativo")
-    }
 
     
+    if(selecionar.value == 1) {
+        entradaValor.value = entradaValor.value * 1
+    } else {
+        entradaValor.value = entradaValor.value * -1
+    }
+
+    var objeto = {descricao: entradaDescricao.value, valor: entradaValor.value}
+
+
+
+    console.log(objeto.descricao, objeto.valor)
+
+  
+
+    
+    const item = document.createElement("li")
+    var itemDescricao = document.createElement("p")
+    itemDescricao = document.createTextNode(objeto.descricao)
+    
+    var itemValor = document.createElement("p")
+    itemValor.classList = "cor"
+
+    itemValor = document.createTextNode(objeto.valor)
+
+  
+
+    item.appendChild(itemDescricao)
+    item.appendChild(itemValor)
+
+    lista.appendChild(item)
+
+    
+    console.log(itemValor.className)
+
+
+    entradaDescricao.value = ""
+    entradaValor.value = ""
 
 })
